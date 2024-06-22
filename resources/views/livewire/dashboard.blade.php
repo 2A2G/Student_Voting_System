218e76c8bcfaa0a49976839a7076ac6105a9ab98
-A L D A I R- <gutierrezga@tecnocomfenalco.edu.co>
2024-06-22 16:04:21 -0500

    Creación del componente usuarios y se añadio al switch de dashboard

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -3,8 +3,16 @@
         <div class="max-w-full mx-auto sm:px-6 lg:px-8">
             <div class="mt-12 ml-4 bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
                 @switch($caso)
-                    @case('usuarios')
+                    @case('rolesPermisos')
                         @livewire('super-admin.roles')
+                    @break
+
+                    @case('usuarios')
+                        @livewire('super-admin.usuarios')
+                    @break
+
+                    @case('dashboard')
+                        @livewire('super-admin.estadistica')
                     @break
 
                     @default
--- resources/views/livewire/sidebar-menu.blade.php
+++ resources/views/livewire/sidebar-menu.blade.php
@@ -86,7 +86,7 @@
             </button>
 
             <div id="menu1" class="flex justify-start  flex-col w-full md:w-auto items-start pb-1 ">
-                <button wire:click="cambiar('usuarios')"
+                <button wire:click="cambiar('rolesPermisos')"
                     class="flex justify-start items-center space-x-6 hover:text-white focus:bg-gray-700 focus:text-white hover:bg-gray-700 text-gray-400 rounded px-3 py-2  w-full md:w-52">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                         <path fill="#dc2626" fill-rule="evenodd"
@@ -95,7 +95,7 @@
                     </svg>
                     <p class="text-base leading-4  ">Roles y permisos</p>
                 </button>
-                <button wire:click="cambiar('name')"
+                <button wire:click="cambiar('usuarios')"
                     class="flex justify-start items-center space-x-6 hover:text-white focus:bg-gray-700 focus:text-white hover:bg-gray-700 text-gray-400 rounded px-3 py-2  w-full md:w-52">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                         <path fill="none" stroke="#dc2626" stroke-linecap="round" stroke-width="2"
